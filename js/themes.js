4e53ccd17e4225fed0d0602dea9e3e9bece7839c
Laura Rodríguez Castillo <laurarodriguezcastillo@Lauras-MacBook-Pro.local>
2025-06-12 08:51:58 +0200

    Apply dark mode

---
+++ js/themes.js
@@ -0,0 +1,17 @@
+/* Apply Themes (light, dark... N themes)*/
+
+function initThemeSwitcher() {
+  const iconDark  = document.querySelector('.icon-dark');
+  const iconLight = document.querySelector('.icon-light');
+
+
+  iconDark.addEventListener('click', () => {
+    document.documentElement.setAttribute('class', 'dark');
+    console.log('Tema: dark');
+  });
+
+  iconLight.addEventListener('click', () => {
+    document.documentElement.setAttribute('class', 'light');
+    console.log('Tema: light');
+  });
+}
